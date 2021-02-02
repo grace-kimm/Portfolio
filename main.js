698e5e9778b98a7a6399005c6010459abe0c1d04
김은혜 <jailke93@gmail.com>
2021-02-02 14:48:37 +0900

    JS: scroll

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+// Handle scrolling when tapping "contact me" button
+const contactButton = document.querySelector(".home__contact");
+contactButton.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
